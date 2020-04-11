f6b0c121d8e5a8fc2fb4a44fee65d92d5443a900
Nicola <niconix.dev@gmail.com>
2020-04-11 14:40:24 +0200

    Add img and circle slider

--- js/main.js
+++ js/main.js
@@ -2,25 +2,64 @@ $(document).ready(function() {
 
     var slideLeft = $('.arrow-left');
     var slideRight = $('.arrow-right');
+    var first = $('.first');
+    var last = $('.last');
+    var firstC = $('.first-circle');
+    var lastC = $('.last-circle');
 
     slideLeft.click(function() {
-        var currentImg = $('.active');
-        var prevImg = currentImg.prev();
-
-        if (prevImg.length) {
-            currentImg.removeClass('active');
-            prevImg.addClass('active');
-        }
-
+        left();
     });
 
     slideRight.click(function() {
-        var currentImg = $('.active');
-        var nextImg = currentImg.next();
+        right();
+    });
 
-        if ((nextImg.length) && (!nextImg.hasClass('slider'))) {
-            currentImg.removeClass('active');
-            nextImg.addClass('active');
+    // Function scorri a sinistra
+    function left() {
+        var currentImg = $('.active');
+        var currentCircle = $('.active-circle')
+
+        currentImg.removeClass('active');
+        currentImg.prev().addClass('active');
+
+        currentCircle.removeClass('active-circle');
+        currentCircle.prev().addClass('active-circle');
+
+
+        if (currentImg.hasClass('first')) {
+            last.addClass('active');
+            lastC.addClass('active-circle')
+        }
+
+    }
+
+
+    // Function scorri a destra
+    function right() {
+        var currentImg = $('.active');
+        var currentCircle = $('.active-circle')
+
+        currentImg.removeClass('active');
+        currentImg.next().addClass('active');
+
+        currentCircle.removeClass('active-circle');
+        currentCircle.next().addClass('active-circle');
+
+
+        if (currentImg.hasClass('last')) {
+            first.addClass('active');
+            firstC.addClass('active-circle');
+        }
+
+    }
+
+    // function sposta le immagini con le frecce
+    $(document).keydown(function(x) {
+        if (x.keyCode == 39) {
+            right();
+        } else if (x.keyCode == 37) {
+            left();
         }
     });
 
